562042859cfb7845fcc116e5997fd604c46aea77
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-08 06:40:26 +0000

    [add] book新規登録機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+    
+  
   def index
   end
 
@@ -9,5 +17,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+    private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
