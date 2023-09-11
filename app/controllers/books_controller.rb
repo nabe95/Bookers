6a246c4a982539b658182e4946869a0663abfc82
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-11 09:48:46 +0000

    [add] 新規登録機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,15 +6,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+      flash[:notice] = "book was succesfully created."
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -24,22 +27,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@book.id)
+      flash[:notice] = "book was succesfully created."
+      redirect_to book_path(@book.id)
     else
     render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "book was succesfully destroyed."
     redirect_to '/books'
   end
-  
+
     private
   #ストロングパラメータ
   def book_params
